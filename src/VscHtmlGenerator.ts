f95dab9ebb9fbf73d29fd384fe767cd56ebea819
nirmalar <nirmalar@microsoft.com>
2016-10-12 12:03:19 +0530

    test build

--- src/VscHtmlGenerator.ts
+++ src/VscHtmlGenerator.ts
@@ -12,8 +12,7 @@ export default class VscHtmlGenerator extends BaseHTMLGenerator {
     constructor() {
         super();
         
-    }
-
+    }    
     protected updateHTML() {
         super.updateHTML();
         let installOptions: string = this.getInstallOptions();
